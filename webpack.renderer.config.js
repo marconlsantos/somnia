2dbf7ff59e9ca69588460f8414f71aa61b1ada0e
Marco Santos <marconlsantos@yahoo.co.uk>
2022-09-05 13:15:28 +0100

    Configuration file tweaking

--- webpack.renderer.config.js
+++ webpack.renderer.config.js
@@ -1,5 +1,4 @@
 const rules = require('./webpack.rules');
-const plugins = require('./webpack.plugins');
 
 rules.push({
   test: /\.css$/,
@@ -10,7 +9,7 @@ module.exports = {
   module: {
     rules,
   },
-  plugins: plugins,
+  plugins: require('./webpack.plugins'),
   resolve: {
     extensions: ['.js', '.ts', '.jsx', '.tsx', '.css'],
   },
