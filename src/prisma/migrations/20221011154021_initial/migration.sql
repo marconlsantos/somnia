edc7aee58fac0cac588ca418e297e49ad5b5bbbb
Marco Santos <marconlsantos@yahoo.co.uk>
2022-10-11 16:51:29 +0100

    Update prisma environment file. Create new initial migration. Move definitions to typings folder.

--- src/prisma/migrations/20221011154021_initial/migration.sql
+++ src/prisma/migrations/20221011154021_initial/migration.sql
@@ -1,25 +1,25 @@
 -- CreateTable
 CREATE TABLE "Dream" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "dreamedAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "title" TEXT NOT NULL,
     "narration" TEXT NOT NULL,
     "interpretation" TEXT
 );
 
 -- CreateTable
-CREATE TABLE "Symbol" (
+CREATE TABLE "DreamSymbol" (
     "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
     "isUniversal" BOOLEAN NOT NULL,
     "referenceCode" TEXT NOT NULL
 );
 
 -- CreateTable
-CREATE TABLE "DreamSymbol" (
+CREATE TABLE "DreamDreamSymbol" (
     "dreamId" INTEGER NOT NULL,
     "symbolId" INTEGER NOT NULL,
 
     PRIMARY KEY ("dreamId", "symbolId"),
-    CONSTRAINT "DreamSymbol_dreamId_fkey" FOREIGN KEY ("dreamId") REFERENCES "Dream" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
-    CONSTRAINT "DreamSymbol_symbolId_fkey" FOREIGN KEY ("symbolId") REFERENCES "Symbol" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
+    CONSTRAINT "DreamDreamSymbol_dreamId_fkey" FOREIGN KEY ("dreamId") REFERENCES "Dream" ("id") ON DELETE RESTRICT ON UPDATE CASCADE,
+    CONSTRAINT "DreamDreamSymbol_symbolId_fkey" FOREIGN KEY ("symbolId") REFERENCES "DreamSymbol" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
 );
